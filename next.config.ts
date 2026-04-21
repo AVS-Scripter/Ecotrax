eb6e56e059216086fa3d11adb4c3415a095fb6cd
Aditya VS <adityasurve251206@gmail.com>
2026-04-21 16:33:02 +0530

    fix: fixed npm run build error when deploying on vercel

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import path from 'path';
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: 'export',
   outputFileTracingRoot: path.join(__dirname),
   typescript: {
     ignoreBuildErrors: true,
